d4829e1ffa324cab905a8e15b22aef1fefc5b202
Andrew Plute <aplute4@gmail.com>
2022-12-06 09:50:45 -0500

    database draft 2 12/6

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, property CASCADE;
+
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -11,4 +12,23 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE property (
+	property_id SERIAL,
+	address varchar(100) NOT NULL,
+	price numeric(12, 2) NOT NULL,
+	bedrooms int NOT NULL,
+	bathrooms numeric (2, 1) NOT NULL,
+	pic_url varchar(250),
+	sq_footage int NOT NULL,
+	description varchar(1000),
+	landlord_id int NOT NULL,
+	CONSTRAINT PK_property PRIMARY KEY (property_id),
+	CONSTRAINT FK_landlord_id_to_user_id FOREIGN KEY (landlord_id) REFERENCES users (user_id),
+	CONSTRAINT CK_price CHECK (price > 0),
+	CONSTRAINT CK_bedrooms CHECK (bedrooms > 0),
+	CONSTRAINT CK_bathrooms CHECK (bathrooms > 0),
+	CONSTRAINT CK_sq_footage CHECK (sq_footage > 0)
+	
+);
+
+COMMIT;
---
+++ capstone/java/src/main/java/com/techelevator/model/Property.java
@@ -0,0 +1,11 @@
+package com.techelevator.model;
+
+public class Property {
+
+    private int PropertyID;
+    private String Address;
+    private int Bedrooms;
+    private double Bathrooms;
+    private String Pic_URL;
+    
+}
