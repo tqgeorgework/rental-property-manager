1371816bfebab96d514fc27afacd8f9db8b204d9
Thomas George <tqgeorgework@gmail.com>
2022-12-06 13:59:45 -0500

    Basic navigation upgrade and UI tweaks

--- capstone/vue/src/main.js
+++ capstone/vue/src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faWrench} from '@fortawesome/free-solid-svg-icons'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faWrench, faSearch, faUserCircle)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
--- capstone/vue/src/views/Listings.vue
+++ capstone/vue/src/views/Listings.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div>Placeholder</div>
 </template>
 
 <script>
