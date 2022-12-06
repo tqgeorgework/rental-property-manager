3493630055854506d4300b34b6cdf3e280f2db03
Andrew Plute <aplute4@gmail.com>
2022-12-06 10:38:55 -0500

    MVC update 1

--- capstone/java/src/main/java/com/techelevator/model/Property.java
+++ capstone/java/src/main/java/com/techelevator/model/Property.java
@@ -2,10 +2,90 @@ package com.techelevator.model;
 
 public class Property {
 
-    private int PropertyID;
-    private String Address;
-    private int Bedrooms;
-    private double Bathrooms;
-    private String Pic_URL;
-    
+    private int propertyID;
+    private String address;
+    private int bedrooms;
+    private double bathrooms;
+    private String picURL;
+    private int sqFootage;
+    private String description;
+
+    public Property() {}
+
+    public Property(int propertyID, String address, int bedrooms, double bathrooms, String picURL, int sqFootage, String description, int landlordID) {
+        this.propertyID = propertyID;
+        this.address = address;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.picURL = picURL;
+        this.sqFootage = sqFootage;
+        this.description = description;
+        this.landlordID = landlordID;
+    }
+
+    public int getPropertyID() {
+        return propertyID;
+    }
+
+    public void setPropertyID(int propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public double getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(double bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public String getPicURL() {
+        return picURL;
+    }
+
+    public void setPicURL(String picURL) {
+        this.picURL = picURL;
+    }
+
+    public int getSqFootage() {
+        return sqFootage;
+    }
+
+    public void setSqFootage(int sqFootage) {
+        this.sqFootage = sqFootage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLandlordID() {
+        return landlordID;
+    }
+
+    public void setLandlordID(int landlordID) {
+        this.landlordID = landlordID;
+    }
+
+    private int landlordID;
 }
