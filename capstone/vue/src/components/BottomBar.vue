6b2e6dde7f1c38d9786a0eebffd910a5ab087367
Thomas George <tqgeorgework@gmail.com>
2022-12-08 15:39:22 -0500

    LinkedIN profiles in bottom bar

--- capstone/vue/src/components/BottomBar.vue
+++ capstone/vue/src/components/BottomBar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    Made By:
+    <a href="https://www.linkedin.com/in/staintonfearon/">Stainton Fearon</a>
+    <a href="https://www.linkedin.com/in/joshua-e-skelly/">Joshua Skelly</a>
+    <a href="https://www.linkedin.com/in/andrew-plute/">Andrew Plute</a>
+    <a href="https://www.linkedin.com/in/ayman-aladhami/">Ayman Aladhami</a>
+    <a href="http://linkedin.com/in/tqgeorgework">TQ George</a>
+  </div>
 </template>
 
 <script>
