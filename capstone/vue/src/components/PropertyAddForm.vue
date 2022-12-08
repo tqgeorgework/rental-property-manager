6a55010e1fdac47ed6d7d32daff055a30ccec431
Josh Skelly <Jskell07@gmail.com>
2022-12-08 10:48:39 -0500

    form_styling

--- capstone/vue/src/components/PropertyAddForm.vue
+++ capstone/vue/src/components/PropertyAddForm.vue
@@ -68,5 +68,33 @@ export default {
 };
 </script>
 
-<style>
+<style scope>
+input {
+  box-sizing: border-box;
+                    font-family: inherit;
+                    font-size: 14px;
+                    vertical-align: baseline;
+                    font-weight: 400;
+                    line-height: 1.29;
+                    letter-spacing: .16px;
+                    border-radius: 0;
+                    outline: 2px solid transparent;
+                    outline-offset: -2px;
+                    
+                    height: 40px;
+                    border: none;
+                    border-bottom: 1px solid #8d8d8d;
+                    background-color: #f4f4f4;
+                    padding: 0 16px;
+                    color: #161616;
+                    transition: background-color 70ms cubic-bezier(.2,0,.38,.9),outline 70ms cubic-bezier(.2,0,.38,.9); 
+}
+                    :focus{
+                        outline: 2px solid #0f62fe;
+                        outline-offset: -2px;
+                    }
+
+form{
+  margin-left: 40%;
+}
 </style>
