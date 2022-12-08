2cac7e2e96bfbc0d6fd96c665b09f490a123bc37
Thomas George <tqgeorgework@gmail.com>
2022-12-08 16:08:10 -0500

    Bottom Bar formatting

--- capstone/vue/src/components/BottomBar.vue
+++ capstone/vue/src/components/BottomBar.vue
@@ -1,20 +1,38 @@
 <template>
-  <div>
-    Made By:
-    <a href="https://www.linkedin.com/in/staintonfearon/">Stainton Fearon</a>
-    <a href="https://www.linkedin.com/in/joshua-e-skelly/">Joshua Skelly</a>
-    <a href="https://www.linkedin.com/in/andrew-plute/">Andrew Plute</a>
-    <a href="https://www.linkedin.com/in/ayman-aladhami/">Ayman Aladhami</a>
-    <a href="http://linkedin.com/in/tqgeorgework">TQ George</a>
+  <div id="bottom">
+    
+    <div id="creators-container">
+      <div>Made By:</div>
+      <div class="creator">
+        <a href="https://www.linkedin.com/in/staintonfearon/">Stainton Fearon</a>
+      </div>
+      <div class="creator">
+        <a href="https://www.linkedin.com/in/joshua-e-skelly/">Joshua Skelly</a>
+      </div>
+      <div class="creator">
+        <a href="https://www.linkedin.com/in/andrew-plute/">Andrew Plute</a>
+      </div>
+      <div class="creator">
+        <a href="https://www.linkedin.com/in/ayman-aladhami/">Ayman Aladhami</a>
+      </div>
+      <div class="creator"><a href="http://linkedin.com/in/tqgeorgework">TQ George</a></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+#bottom {
+  border: 2px lightgray solid;
+  padding: 10px 30px 10px 30px;
+}
+#creators-container {
+  display: flex;
+  width: 75vw;
+  justify-content: space-between;
 
+}
 </style>
